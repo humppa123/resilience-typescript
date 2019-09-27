3f35d8ba7646ea0ac18f0ddb05a886e3d377778d
Fabian Schwarz <f.schwarz@digital-ratio.de>
2019-09-27 11:30:03 +0200

    Updated package and readme

--- src/test/tokenCache/tokenCache.spec.ts
+++ src/test/tokenCache/tokenCache.spec.ts
@@ -13,22 +13,6 @@ const expect = chai.expect;
 describe("Resilence", () => {
     describe("Token Cache", () => {
         const logger = new NoLogger();
-        it("Should return a proper AAD token", async () => {
-            // Arrange
-            const baseUrl = "https://login.microsoftonline.com";
-            const clientId = "b8aa2950-7bfe-41c4-bba0-4b134f755378";
-            const clientSecret = "EzKAq4WTcyIkLzxr4wjPuKc7LQMPIZfjdlHcw7lKsVo=";
-            const tenantId = "0c38f499-ffed-49dc-a319-60b30fb03e68";
-            const provider: ITokenProvider = new AzureActiveDirectoryAppRegistrationTokenProvider(baseUrl, clientId, clientSecret, tenantId, logger);
-
-            // Act
-            const result = await provider.getToken();
-
-            // Assert
-            expect(result.accessToken).to.not.equal("");
-            expect(result.expires).to.not.equal(0);
-        });
-
         it("Should query token provider for a token on first call", async () => {
             // Arrange
             const accessToken = "This is the access token";
