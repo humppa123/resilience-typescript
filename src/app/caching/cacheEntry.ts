abf5961dc93bfc06264739105d05cc34d1d2d9be
Fabian Schwarz <f.schwarz@digital-ratio.de>
2019-10-07 16:28:29 +0200

    Simplified memory cache by using a new cache entry model.

---
+++ src/app/caching/cacheEntry.ts
@@ -0,0 +1,35 @@
+import { Guard } from "../utils/guard";
+
+/**
+ * A entry for the a cache.
+ */
+export class CacheEntry<TKey, TValue> {
+    /**
+     * Gets or sets the expiration timestamp for this cache entry.
+     */
+    public expiration: Date;
+    /**
+     * Gets or sets the key of the cache entry.
+     */
+    public key: TKey;
+    /**
+     * Gets or sets the value of the cache entry.
+     */
+    public value: TValue;
+
+    /**
+     * Initializes a new instance of the @see CacheEntry class.
+     * @param key Key of the cache entry.
+     * @param value Value of the cache entry.
+     * @param expiration Expiraiton date for cache entry.
+     */
+    constructor(key: TKey, value: TValue, expiration: Date) {
+        Guard.throwIfNullOrEmpty(key, "key");
+        Guard.throwIfNullOrEmpty(value, "value");
+        Guard.throwIfNullOrEmpty(expiration, "expiration");
+
+        this.expiration = expiration;
+        this.key = key;
+        this.value = value;
+    }
+}
